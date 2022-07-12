3110b5f950053a897a6ffff5aaf5c04820356ae6
Florian Kattner <florian.kattner@gmail.com>
2022-07-12 17:48:17 +0200

    Add option to reset grid size using prompt

--- app.js
+++ app.js
@@ -1,9 +1,31 @@
 const gridContainer = document.querySelector('.main-grid');
+const dimensionButton = document.querySelector('.change-dimensions');
+let dimensions;
+
 let clickStarted = false;
+
 window.addEventListener('mouseup', () => clickStarted = false);
+dimensionButton.addEventListener('click', changeDimensions);
+
+function changeDimensions(){
+    let validInput = false;
+
+    while(!validInput) {
+        dimensions = parseInt(prompt("Enter the new dimension (max 100)", 16));
+        if((dimensions > 0 && dimensions <= 100)){
+            validInput = true;
+        }
+    }
+    createGrid(dimensions, dimensions);
+}
+
+function resetGrid(){
+    let allTiles = gridContainer.querySelectorAll(".grid-item");
+    allTiles.forEach((tile) => tile.remove());
+}
 
 function createGrid(rows, columns){
-
+    resetGrid();
     for (let i = rows; i > 0; i--) {
         for (let j = columns; j > 0; j--) {
             let div = document.createElement("div");
