8e600d5ee207bb69e8a2c418e3c1b8d58d201fa7
Florian Kattner <florian.kattner@gmail.com>
2022-07-12 17:07:09 +0200

    Squares turn red by clicking them (including click and hover)

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 const gridContainer = document.querySelector('.main-grid');
+let clickStarted = false;
 
 function createGrid(rows, columns){
 
     for (let i = rows; i > 0; i--) {
         for (let j = columns; j > 0; j--) {
             let div = document.createElement("div");
+
+            div.addEventListener('mousedown', () => { clickStarted = true; makeRed(div) })
+            div.addEventListener('mouseup', () => clickStarted = false)
+            div.addEventListener('mouseover', () => { if(clickStarted) makeRed(div) })
+
             div.classList.add('grid-item');
             gridContainer.append(div);
         } 
@@ -13,5 +19,10 @@ function createGrid(rows, columns){
     gridContainer.style.gridTemplateRows = `repeat(${columns}, 1fr)`;
 
 }
+
+function makeRed(clickedDiv){
+    clickedDiv.classList.add("red-bg");
+}
+
 
 createGrid(16, 16);
